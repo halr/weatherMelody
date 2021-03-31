78f01330a8caf4cd0cbde80138c5b8c4cf930707
Hal Rodriguez <halr66@gmail.com>
2021-03-31 15:04:36 -0700

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-//arduino_secrets.h
-char ssid[] = "malloup2";             // network SSID (name)
-char pass[] = "diamoniakcarcassonne"; // network password (use for WPA, or use as key for WEP)
-//int keyIndex = 0;                     // network key Index number (needed only for WEP)
